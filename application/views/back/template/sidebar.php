4c56e584df841d31d11b2f33828578fefe307eab
Firmansyah SAL <syah.firman55@gmail.com>
2021-03-26 14:54:32 +0700

    merapikan halaman administrator

--- application/views/back/template/sidebar.php
+++ application/views/back/template/sidebar.php
@@ -4,19 +4,24 @@
     <div class="container">
       <ul class="mainnav">
         <li class="active"><a href="index.html"><i class="icon-dashboard"></i><span>Dashboard</span> </a> </li>
-        <li><a href="<?= base_url() ?>assets/back/reports.html"><i class="icon-list-alt"></i><span>Laporan</span> </a> </li>
-        <li><a href="<?= base_url() ?>assets/back/charts.html"><i class="icon-bar-chart"></i><span>Statistik</span> </a> </li>
-        <li><a href="<?= base_url() ?>assets/back/shortcodes.html"><i class="icon-code"></i><span>Shortcodes</span> </a> </li>
-        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-long-arrow-down"></i><span>Drops</span> <b class="caret"></b></a>
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-folder-close"></i><span>Data Master</span> <b class="caret"></b></a>
           <ul class="dropdown-menu">
-            <li><a href="<?= base_url() ?>assets/back/icons.html">Icons</a></li>
-            <li><a href="<?= base_url() ?>assets/back/faq.html">FAQ</a></li>
-            <li><a href="<?= base_url() ?>assets/back/pricing.html">Pricing Plans</a></li>
-            <li><a href="<?= base_url() ?>assets/back/login.html">Login</a></li>
-            <li><a href="<?= base_url() ?>assets/back/signup.html">Signup</a></li>
-            <li><a href="<?= base_url() ?>assets/back/error.html">404</a></li>
+            <li><a href="<?= base_url() ?>assets/back/icons.html">Data Divisi</a></li>
+            <li><a href="<?= base_url() ?>assets/back/faq.html">Data jabatan</a></li>
+            <li><a href="<?= base_url() ?>assets/back/pricing.html">Data Karyawan</a></li>
           </ul>
         </li>
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-credit-card"></i><span>Master Ticket</span> <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="<?= base_url() ?>assets/back/icons.html">Data Ticket</a></li>
+          </ul>
+        </li>
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-list-alt"></i><span>Laporan Ticket</span> <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="<?= base_url() ?>assets/back/icons.html">Laporan</a></li>
+          </ul>
+        </li>
+        <li><a href="<?= base_url() ?>assets/back/shortcodes.html"><i class="icon-code"></i><span>Shortcodes</span> </a> </li>
       </ul>
     </div>
     <!-- /container -->
