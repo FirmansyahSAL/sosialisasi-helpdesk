93e244de58a562efb95efcd706c9cc68490847d7
Firmansyah SAL <syah.firman55@gmail.com>
2021-03-27 11:37:10 +0700

    merubah tampilan menu dashboard admin

--- application/views/back/template/sidebar.php
+++ application/views/back/template/sidebar.php
@@ -6,14 +6,14 @@
         <li class="active"><a href="index.html"><i class="icon-dashboard"></i><span>Dashboard</span> </a> </li>
         <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-folder-close"></i><span>Data Master</span> <b class="caret"></b></a>
           <ul class="dropdown-menu">
-            <li><a href="<?= base_url() ?>assets/back/icons.html">Data Divisi</a></li>
-            <li><a href="<?= base_url() ?>assets/back/faq.html">Data jabatan</a></li>
-            <li><a href="<?= base_url() ?>assets/back/pricing.html">Data Karyawan</a></li>
+            <li><a href="<?= base_url() ?>Jabatan">Data Jabatan</a></li>
+            <li><a href="<?= base_url() ?>Divisi">Data Divisi</a></li>
+            <li><a href="<?= base_url() ?>Karyawan">Data Karyawan</a></li>
           </ul>
         </li>
         <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-credit-card"></i><span>Master Ticket</span> <b class="caret"></b></a>
           <ul class="dropdown-menu">
-            <li><a href="<?= base_url() ?>assets/back/icons.html">Data Ticket</a></li>
+            <li><a href="<?= base_url() ?>Ticket">Data Ticket</a></li>
           </ul>
         </li>
         <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-list-alt"></i><span>Laporan Ticket</span> <b class="caret"></b></a>
