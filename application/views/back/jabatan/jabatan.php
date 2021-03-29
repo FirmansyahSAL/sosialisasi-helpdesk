f5c13e528d2473c43570c34dc18984dc6bc0e861
Firmansyah SAL <syah.firman55@gmail.com>
2021-03-29 14:31:27 +0700

    tampilan form jabatan

--- application/views/back/jabatan/jabatan.php
+++ application/views/back/jabatan/jabatan.php
@@ -1,51 +1,132 @@
-<div class="main">
-    <div class="main-inner">
-        <div class="container">
-            <div class="widget widget-table action-table">
-                <div class="widget-header"> <i class="icon-th-list"></i>
-                    <h3>A Table Example</h3>
-                </div>
-                <!-- /widget-header -->
-                <div class="widget-content">
-                    <table class="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <th> Free Resource </th>
-                                <th> Download</th>
-                                <th class="td-actions"> </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td> Fresh Web Development Resources </td>
-                                <td> http://www.egrappler.com/ </td>
-                                <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                            </tr>
-                            <tr>
-                                <td> Fresh Web Development Resources </td>
-                                <td> http://www.egrappler.com/ </td>
-                                <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                            </tr>
-                            <tr>
-                                <td> Fresh Web Development Resources </td>
-                                <td> http://www.egrappler.com/ </td>
-                                <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                            </tr>
-                            <tr>
-                                <td> Fresh Web Development Resources </td>
-                                <td> http://www.egrappler.com/ </td>
-                                <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                            </tr>
-                            <tr>
-                                <td> Fresh Web Development Resources </td>
-                                <td> http://www.egrappler.com/ </td>
-                                <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                            </tr>
+<div class="subnavbar">
 
-                        </tbody>
-                    </table>
+    <div class="container">
+        <div class="row">
+
+            <div class="span12">
+
+                <div class="widget ">
+                    <div class="widget-header"> <i class="icon-th-list"></i>
+                        <h3>Form Jabatan</h3>
+                    </div>
+
+                    <div class="widget-content">
+
+                        <a href="http://localhost/sosialisasi2/index.php/admin/tambahpublikasi"><button type="button" class="btn btn-primary">Tambah Data</button></a>
+
+                        <p>&nbsp;</p>
+
+
+
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-hovered">
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Jabatan</th>
+                                    <th>action</th>
+
+                                </tr>
+                                <tbody id="data">
+                                    <tr>
+                                        <td>139</td>
+                                        <td>another goal</td>
+
+                                        <td>
+                                            <a href="http://localhost/sosialisasi2/admin/editpublikasi/139"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                            <a href="http://localhost/sosialisasi2/admin/deletepublikasi/139"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>140</td>
+                                        <td>postinor</td>
+
+
+                                        <td>
+                                            <a href="http://localhost/sosialisasi2/admin/editpublikasi/140"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                            <a href="http://localhost/sosialisasi2/admin/deletepublikasi/140"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>141</td>
+                                        <td>hagia sovie</td>
+
+
+                                        <td>
+                                            <a href="http://localhost/sosialisasi2/admin/editpublikasi/141"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                            <a href="http://localhost/sosialisasi2/admin/deletepublikasi/141"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>143</td>
+                                        <td>amd</td>
+
+
+                                        <td>
+                                            <a href="http://localhost/sosialisasi2/admin/editpublikasi/143"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                            <a href="http://localhost/sosialisasi2/admin/deletepublikasi/143"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>144</td>
+                                        <td>intel</td>
+
+
+                                        <td>
+                                            <a href="http://localhost/sosialisasi2/admin/editpublikasi/144"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                            <a href="http://localhost/sosialisasi2/admin/deletepublikasi/144"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>145</td>
+                                        <td>whatde</td>
+
+
+                                        <td>
+                                            <a href="http://localhost/sosialisasi2/admin/editpublikasi/145"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                            <a href="http://localhost/sosialisasi2/admin/deletepublikasi/145"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>147</td>
+                                        <td>ya tuhan</td>
+
+
+                                        <td>
+                                            <a href="http://localhost/sosialisasi2/admin/editpublikasi/147"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                            <a href="http://localhost/sosialisasi2/admin/deletepublikasi/147"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>148</td>
+                                        <td>filehelp</td>
+
+
+                                        <td>
+                                            <a href="http://localhost/sosialisasi2/admin/editpublikasi/148"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                            <a href="http://localhost/sosialisasi2/admin/deletepublikasi/148"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>151</td>
+                                        <td>database MYSQL</td>
+
+
+
+                                        <td>
+                                            <a href="http://localhost/sosialisasi2/admin/editpublikasi/151"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                            <a href="http://localhost/sosialisasi2/admin/deletepublikasi/151"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                        </td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+
+                            <p class='paging' align='center'> </p>
+                        </div>
+
+
+                    </div>
                 </div>
-                <!-- /widget-content -->
             </div>
         </div>
     </div>
