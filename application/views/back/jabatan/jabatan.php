a3ffb4b3c6d5cbea44ec129990c22f2d7c615800
Firmansyah SAL <syah.firman55@gmail.com>
2021-04-01 14:13:56 +0700

    membuat form jabatan

--- application/views/back/jabatan/jabatan.php
+++ application/views/back/jabatan/jabatan.php
@@ -16,21 +16,17 @@
 
                         <p>&nbsp;</p>
 
-
-
                         <div class="table-responsive">
                             <table class="table table-bordered table-hovered">
                                 <tr>
                                     <th>No</th>
                                     <th>Nama Jabatan</th>
                                     <th>action</th>
-
                                 </tr>
                                 <tbody id="data">
                                     <tr>
                                         <td>139</td>
                                         <td>another goal</td>
-
                                         <td>
                                             <a href="http://localhost/sosialisasi2/admin/editpublikasi/139"><button type='button' class='btn btn-success'>Ubah</button></a>
                                             <a href="http://localhost/sosialisasi2/admin/deletepublikasi/139"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
@@ -39,8 +35,6 @@
                                     <tr>
                                         <td>140</td>
                                         <td>postinor</td>
-
-
                                         <td>
                                             <a href="http://localhost/sosialisasi2/admin/editpublikasi/140"><button type='button' class='btn btn-success'>Ubah</button></a>
                                             <a href="http://localhost/sosialisasi2/admin/deletepublikasi/140"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
@@ -49,8 +43,6 @@
                                     <tr>
                                         <td>141</td>
                                         <td>hagia sovie</td>
-
-
                                         <td>
                                             <a href="http://localhost/sosialisasi2/admin/editpublikasi/141"><button type='button' class='btn btn-success'>Ubah</button></a>
                                             <a href="http://localhost/sosialisasi2/admin/deletepublikasi/141"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
@@ -59,8 +51,6 @@
                                     <tr>
                                         <td>143</td>
                                         <td>amd</td>
-
-
                                         <td>
                                             <a href="http://localhost/sosialisasi2/admin/editpublikasi/143"><button type='button' class='btn btn-success'>Ubah</button></a>
                                             <a href="http://localhost/sosialisasi2/admin/deletepublikasi/143"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
@@ -69,8 +59,6 @@
                                     <tr>
                                         <td>144</td>
                                         <td>intel</td>
-
-
                                         <td>
                                             <a href="http://localhost/sosialisasi2/admin/editpublikasi/144"><button type='button' class='btn btn-success'>Ubah</button></a>
                                             <a href="http://localhost/sosialisasi2/admin/deletepublikasi/144"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
@@ -79,8 +67,6 @@
                                     <tr>
                                         <td>145</td>
                                         <td>whatde</td>
-
-
                                         <td>
                                             <a href="http://localhost/sosialisasi2/admin/editpublikasi/145"><button type='button' class='btn btn-success'>Ubah</button></a>
                                             <a href="http://localhost/sosialisasi2/admin/deletepublikasi/145"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
@@ -89,8 +75,6 @@
                                     <tr>
                                         <td>147</td>
                                         <td>ya tuhan</td>
-
-
                                         <td>
                                             <a href="http://localhost/sosialisasi2/admin/editpublikasi/147"><button type='button' class='btn btn-success'>Ubah</button></a>
                                             <a href="http://localhost/sosialisasi2/admin/deletepublikasi/147"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
@@ -99,8 +83,6 @@
                                     <tr>
                                         <td>148</td>
                                         <td>filehelp</td>
-
-
                                         <td>
                                             <a href="http://localhost/sosialisasi2/admin/editpublikasi/148"><button type='button' class='btn btn-success'>Ubah</button></a>
                                             <a href="http://localhost/sosialisasi2/admin/deletepublikasi/148"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
@@ -109,22 +91,15 @@
                                     <tr>
                                         <td>151</td>
                                         <td>database MYSQL</td>
-
-
-
                                         <td>
                                             <a href="http://localhost/sosialisasi2/admin/editpublikasi/151"><button type='button' class='btn btn-success'>Ubah</button></a>
                                             <a href="http://localhost/sosialisasi2/admin/deletepublikasi/151"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
                                         </td>
                                     </tr>
-
                                 </tbody>
                             </table>
-
                             <p class='paging' align='center'> </p>
                         </div>
-
-
                     </div>
                 </div>
             </div>
