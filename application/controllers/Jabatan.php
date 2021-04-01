e1d9f46d3ad46df182fe3ea7ac1eb6d7fba5e90d
Firmansyah SAL <syah.firman55@gmail.com>
2021-04-01 15:15:56 +0700

    menampilkan data di halaman jabatan final

--- application/controllers/Jabatan.php
+++ application/controllers/Jabatan.php
@@ -5,7 +5,6 @@ class Jabatan extends CI_Controller
 {
     public function index()
     {
-        $this->load->model('M_jabatan');
         $data['jabatan'] = $this->M_jabatan->get_jabatan();
 
         $this->template->load('back/template', 'back/jabatan/jabatan', $data);
