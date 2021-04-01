7ab4813a3f809b4332f1556450f1f8ac799a0fe8
Firmansyah SAL <syah.firman55@gmail.com>
2021-04-01 14:18:30 +0700

    tampilan dashboard halaman utama admin responsive

--- application/views/back/dashboard.php
+++ application/views/back/dashboard.php
@@ -1,57 +1,58 @@
-<div class="main-inner">
+<div class="subnavbar">
+
   <div class="container">
     <div class="row">
-      <div class="span6">
-        <div class="widget widget-nopad">
-          <div class="widget-header"> <i class="icon-list-alt"></i>
-            <h3> Statistik </h3>
-          </div>
-          <!-- /widget-header -->
+
+      <div class="span12">
+
+        <div class="widget ">
+          <div class="widget-header"> <i class="icon-th-list"></i></div>
+
           <div class="widget-content">
-            <div class="widget big-stats-container">
-              <div class="widget-content">
 
-                <h6 class="bigstats">Selamat datang <?= $this->session->username; ?></h6>
+            <div class="span5">
+              <div class="widget widget-nopad">
+                <div class="widget-header"> <i class="icon-list-alt"></i>
+                  <h3>Statistik</h3>
+                </div>
+                <!-- /widget-header -->
+                <div class="widget-content">
+                  <div class="widget big-stats-container">
 
+                    <h6 class="bigstats">Selamat Datang <?= $this->session->username; ?></h6>
+                    <div id="big_stats" class="cf">
+                      <div class="stat"> <i class="icon-anchor"></i> <span class="value">851</span> </div>
+                      <!-- .stat -->
 
-                <div id="big_stats" class="cf">
-                  <div class="stat"> <i class="icon-user"></i> <span class="value">851</span> </div>
-                  <!-- .stat -->
+                      <div class="stat"> <i class="icon-thumbs-up-alt"></i> <span class="value">423</span> </div>
+                      <!-- .stat -->
 
-                  <div class="stat"> <i class="icon-thumbs-up-alt"></i> <span class="value">423</span> </div>
-                  <!-- .stat -->
+                      <div class="stat"> <i class="icon-twitter-sign"></i> <span class="value">922</span> </div>
+                      <!-- .stat -->
 
-                  <div class="stat"> <i class="icon-twitter-sign"></i> <span class="value">922</span> </div>
-                  <!-- .stat -->
-
-                  <div class="stat"> <i class="icon-bullhorn"></i> <span class="value">25%</span> </div>
-                  <!-- .stat -->
+                      <div class="stat"> <i class="icon-bullhorn"></i> <span class="value">25%</span> </div>
+                      <!-- .stat -->
+                    </div>
+                  </div>
+                  <!-- /widget-content -->
                 </div>
               </div>
-              <!-- /widget-content -->
+            </div>
+
+            <div class="span6">
+              <div class="widget-header"> <i class="icon-signal"></i>
+                <h3>Grafik</h3>
+              </div>
+              <!-- /widget-header -->
+              <div class="widget-content">
+                <canvas id="area-chart" class="chart-holder" height="250" width="530"> </canvas>
+              </div>
             </div>
           </div>
+          <p>&nbsp;</p>
         </div>
       </div>
-      <div class="row">
-        <div class="span6">
-          <div class="widget">
-            <div class="widget-header"> <i class="icon-signal"></i>
-              <h3> Area Chart Example</h3>
-            </div>
-            <!-- /widget-header -->
-            <div class="widget-content">
-              <canvas id="area-chart" class="chart-holder" height="250" width="538"> </canvas>
-              <!-- /area-chart -->
-            </div>
-
-          </div>
-
-        </div>
-
-      </div>
-
     </div>
-
   </div>
+</div>
 </div>
--- application/views/back/template.php
+++ application/views/back/template.php
@@ -1,9 +1,9 @@
     <?php $this->load->view('back/template/meta'); ?>
-    <div class="main">
+   
         <?php $this->load->view('back/template/header'); ?>
         <?php $this->load->view('back/template/sidebar'); ?>
         <?php echo $contents; ?>
-        <?php $this->load->view('back/template/footer'); ?>
 
-        <?php $this->load->view('back/template/script'); ?>
-    </div>
+        <?php $this->load->view('back/template/footer'); ?>
+   
+    <?php $this->load->view('back/template/script'); ?>
