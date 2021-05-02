f7e731504fd89d689b945dc7f74a8ccee83233da
Firmansyah SAL <syah.firman55@gmail.com>
2021-05-02 12:57:34 +0700

    fix halaman divisi done

--- application/views/back/divisi/data_divisi.php
+++ application/views/back/divisi/data_divisi.php
@@ -13,38 +13,38 @@
 
 
                     <div class="widget-content">
-                        <a href="<?= base_url('divisi/add_divisi') ?>"><button type="button" class="btn btn-primary">Tambah Data</button></a>
-                        <p>&nbsp;</p>
-
-                        <?= $this->session->flashdata('message'); ?>
-
                         <div class="span11">
+                            <a href="<?= base_url('divisi/add_divisi') ?>"><button type="button" class="btn btn-primary">Tambah Data</button></a>
+                            <div class="control-group">
+                                <?= $this->session->flashdata('message'); ?>
 
-                            <p>&nbsp;</p>
-                            <div class="table-responsive">
-                                <table class="table table-bordered table-hovered">
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nama Divisi</th>
-                                        <th>action</th>
-                                    </tr>
-                                    <tbody>
-                                        <?php
-                                        $no = 1;
-                                        foreach ($divisi as $row) { ?>
-                                            <tr>
-                                                <td><?= $no++ ?></td>
-                                                <td><?= $row->divisi ?></td>
-                                                <td>
-                                                    <a href="<?= base_url('divisi/edit_divisi/' . $row->id_divisi) ?>"><button type='button' class='btn btn-success'>Ubah</button></a>
-                                                    <a href="<?= base_url('divisi/delete_divisi/' . $row->id_divisi) ?>"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
-                                                </td>
-                                            </tr>
-                                        <?php } ?>
 
-                                    </tbody>
-                                </table>
-                                <p class='paging' align='center'> </p>
+                                <p>&nbsp;</p>
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-hovered">
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama Divisi</th>
+                                            <th>action</th>
+                                        </tr>
+                                        <tbody>
+                                            <?php
+                                            $no = 1;
+                                            foreach ($divisi as $row) { ?>
+                                                <tr>
+                                                    <td><?= $no++ ?></td>
+                                                    <td><?= $row->divisi ?></td>
+                                                    <td>
+                                                        <a href="<?= base_url('divisi/edit_divisi/' . $row->id_divisi) ?>"><button type='button' class='btn btn-success'>Ubah</button></a>
+                                                        <a href="<?= base_url('divisi/delete_divisi/' . $row->id_divisi) ?>"><button type='button' class='btn btn-danger' onclick="return confirm('Are you sure to delete?')">Hapus</button></a>
+                                                    </td>
+                                                </tr>
+                                            <?php } ?>
+
+                                        </tbody>
+                                    </table>
+                                    <p class='paging' align='center'> </p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -52,4 +52,3 @@
             </div>
         </div>
     </div>
-</div>
